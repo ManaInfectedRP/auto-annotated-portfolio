cc81d1f7177def2b6cab7826ed80daac72f12e36
ManaInfectedRP <52261763+ManaInfectedRP@users.noreply.github.com>
2026-01-25 23:30:47 +0100

    test

--- src/utils/highlighted-markdown.tsx
+++ src/utils/highlighted-markdown.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
-import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css';
-import { funky } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter/dist/esm/prism-light';
+import js from 'react-syntax-highlighter/dist/esm/languages/prism/javascript';
+import css from 'react-syntax-highlighter/dist/esm/languages/prism/css';
+import { funky } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 SyntaxHighlighter.registerLanguage('javascript', js);
 SyntaxHighlighter.registerLanguage('css', css);
